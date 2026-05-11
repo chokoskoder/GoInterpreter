304596500b0337e00c026ed8f1fc65ee719d49fc
Siddhant Chopra <chokoskoder@gmail.com>
2026-05-11 05:39:40 +0530

    the repl works and now moving onto ast+parser

---
+++ src/monkey/repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"github.com/chokoskoder/GoInterpreter/lexer"
+	"github.com/chokoskoder/GoInterpreter/token"
+)
+
+
+
+const PROMPT = ">>"
+
+func Start(in io.Reader, out io.Writer){
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		for tok := l.Scan(); tok.Type != token.EOF; tok = l.Scan() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
